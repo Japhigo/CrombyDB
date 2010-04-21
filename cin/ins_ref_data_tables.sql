ae436ace619d5792a56d9639815e62aff005a994
Phil Goldsbrough <phil.goldsbrough@btinternet.com>
2010-04-21 20:01:21 +0100

    Ref Data UI Prototype

--- cin/ins_ref_data_tables.sql
+++ cin/ins_ref_data_tables.sql
@@ -3,16 +3,16 @@
 select rlm.register_component ( 'CIN', 'ins_ref_data_tables.sql' );
 
 begin;
-insert into cin.ref_data_tables ( table_name, code_mandatory, code_min_length, code_max_length, code_format, display_mandatory, description_mandatory)
-  values ( 'contact_point_usage_types', false, 0, 0, null, true, false );
-insert into cin.ref_data_tables ( table_name, code_mandatory, code_min_length, code_max_length, code_format, display_mandatory, description_mandatory)
-  values ( 'employment_statuses', true, 2, 3, null, true, true );
-insert into cin.ref_data_tables ( table_name, code_mandatory, code_min_length, code_max_length, code_format, display_mandatory, description_mandatory)
-  values ( 'genders', true, 1, 1, null, true, true );
-insert into cin.ref_data_tables ( table_name, code_mandatory, code_min_length, code_max_length, code_format, display_mandatory, description_mandatory)
-  values ( 'marital_statuses', true, 1, 1, null, true, true );
-insert into cin.ref_data_tables ( table_name, code_mandatory, code_min_length, code_max_length, code_format, display_mandatory, description_mandatory)
-  values ( 'match_statuses', true, 1, 1, null, true, true );
+insert into cin.ref_data_tables ( table_name, description, code_mandatory, code_min_length, code_max_length, code_format, display_mandatory, description_mandatory)
+  values ( 'contact_point_usage_types', 'Indicates the type of use for a contact point', false, 0, 0, null, true, false );
+insert into cin.ref_data_tables ( table_name,  description, code_mandatory, code_min_length, code_max_length, code_format, display_mandatory, description_mandatory)
+  values ( 'employment_statuses', 'An individual''s employment status', true, 2, 3, null, true, true );
+insert into cin.ref_data_tables ( table_name,  description, code_mandatory, code_min_length, code_max_length, code_format, display_mandatory, description_mandatory)
+  values ( 'genders', 'Gender (aka sex) of an individual', true, 1, 1, null, true, true );
+insert into cin.ref_data_tables ( table_name,  description, code_mandatory, code_min_length, code_max_length, code_format, display_mandatory, description_mandatory)
+  values ( 'marital_statuses', 'Marital status of an individual', true, 1, 1, null, true, true );
+insert into cin.ref_data_tables ( table_name,  description, code_mandatory, code_min_length, code_max_length, code_format, display_mandatory, description_mandatory)
+  values ( 'match_statuses', 'Indicates whether an address has been matched against an external database of addresses e.g. PAF', true, 1, 1, null, true, true );
 
 commit;
 
--- cin/tab_ref_data_tables.sql
+++ cin/tab_ref_data_tables.sql
@@ -5,6 +5,7 @@ select rlm.register_component ( 'CIN', 'tab_ref_data_tables.sql' );
 create table cin.ref_data_tables
   ( id                              serial                 not null primary key
   , table_name                      varchar(63)            not null
+  , description                     varchar(255)           not null
   , code_mandatory                  boolean                not null default false
   , code_min_length                 smallint               not null default 0
   , code_max_length                 smallint               not null default 0
