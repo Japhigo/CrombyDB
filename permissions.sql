3c99a6d826158871f62234178b7ad46aca67ea64
Phil Goldsbrough <phil.goldsbrough@btinternet.com>
2011-03-23 19:38:53 +0000

    Work on permissions

--- permissions.sql
+++ permissions.sql
@@ -10,15 +10,36 @@ create role ref_data_admin nologin inherit;
 
 grant select, insert, update on cin.business_types to ref_data_admin;
 grant select, insert, update on cin.communication_languages to ref_data_admin;
+grant select, insert, update on cin.contact_point_fail_types to ref_data_admin;
 grant select, insert, update on cin.contact_point_usage_types to ref_data_admin;
 grant select, insert, update on cin.countries to ref_data_admin;
 grant select on cin.data_load_statuses to ref_data_admin;
 grant select on cin.data_statuses to ref_data_admin;
 grant select, insert, update on cin.employment_statuses to ref_data_admin;
+grant select, insert, update on cin.event_role_types to ref_data_admin;	
+grant select, insert, update on cin.event_statuses to ref_data_admin;	
+grant select, insert, update on cin.event_types to ref_data_admin;	
+grant select, insert, update on cin.flagged_address_reasons to ref_data_admin;	
 grant select, insert, update on cin.genders to ref_data_admin;
+grant select, insert, update on cin.legal_classifications to ref_data_admin;	
 grant select, insert, update on cin.marital_statuses to ref_data_admin;
 grant select, insert, update on cin.match_statuses to ref_data_admin;
+grant select, insert, update on cin.occupation_types to ref_data_admin;	
+grant select, insert, update on cin.party_classifications to ref_data_admin;	
+grant select, insert, update on cin.party_group_types to ref_data_admin;	
+grant select, insert, update on cin.party_role_types to ref_data_admin;	
+grant select, insert, update on cin.party_types to ref_data_admin;
+grant select, insert, update on cin.postal_address_categories to ref_data_admin;	
+grant select, insert, update on cin.ref_data_load_rows to ref_data_admin;
+grant select, insert, update on cin.ref_data_loads to ref_data_admin;
 grant select, update on cin.ref_data_tables to ref_data_admin;
+grant select, insert, update on cin.relationship_priorities to ref_data_admin;
+grant select, insert, update on cin.relationship_statuses to ref_data_admin;
+grant select, insert, update on cin.relationship_types to ref_data_admin;
+grant select, insert, update on cin.telephone_connection_types to ref_data_admin;
+
+
+grant ref_data_admin to am_user;
 
 create role employee_admin nologin inherit;
 create role security_admin nologin inherit;
